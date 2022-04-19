6334a74c4dccfff11f0b14543eb5a1e078175f2a
1Knightz <103395667+1Knightz@users.noreply.github.com>
2022-04-19 16:00:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+       function cal(){
+           var nums = document.getElementsByName("num");
+           var sz = document.getElementsByName("js");
+           var result = document.getElementsByName("rs");
+           var n1 = parseFloat(nums[0].value);
+           var n2 = parseFloat(nums[1].value);
+           /*parseFloat()
+        */
+            switch(sz[0].value){
+            case "add" : 
+            result[0].value = n1 + n2 ;
+            break;
+              }
+          }
+      </script>
+                Knight ChengJiahao
+                define variable
